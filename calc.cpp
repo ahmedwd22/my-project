417746fe4b7c814ed9ad453177f3de41b2e15ac1
ahmed <ahmedwd2004@gmail.com>
2023-04-06 04:36:58 +0200

    calc.cpp

---
+++ calc.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+     int x,y,sum,z;
+     cout<<"enter x,y\n";
+     cin>>x ;
+     cin>>y;
+     sum = x+y;
+     cout<<"x+y=..\n" ;
+     cout<<sum;
+    return 0;
+}
